7ad617c0be53f73447818b56854f3a51d41c4825
Rehan <rehnx95@gmail.com>
2026-08-08 02:21:44 +0530

    Learning First Principle

---
+++ 2 - First Principle/19-httpServer.js
@@ -0,0 +1,28 @@
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+
+const port = process.env.PORT || 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  const html = fs.readFileSync(path.join(__dirname, "idx.html"), "utf8");
+  res.setHeader("Content-Type", "text/html");
+  console.log(req.url);
+  if (req.url === "/") {
+    res.end(html);
+  } else if (req.url === "/about") {
+    res.end("  <h1>Welcome to My Website</h1> <p>My Website About </p>");
+  } else if (req.url === "/home") {
+    res.end(html);
+  } else if (req.url === "/services") {
+    res.end("<h1>Welcome to My Website</h1> <p>My Website Service</p>");
+  } else if (req.url === "/contact") {
+    res.end("<h1>Welcome to My Website</h1>  <p>My Website Contact</p>");
+  } else {
+    res.end("<p>404 Page not found</p>");
+  }
+});
+
+server.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running on Port ${port}`);
+});
