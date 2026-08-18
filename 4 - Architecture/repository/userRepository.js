a9971d31da330aaf5ed203a26a0eedd631626ead
Rehan <rehnx95@gmail.com>
2026-08-18 20:58:34 +0530

    Updating CodeBase

--- 4 - Architecture/repository/userRepository.js
+++ 4 - Architecture/repository/userRepository.js
@@ -13,28 +13,3 @@ function count() {
 }
 module.exports = { findByEmail, create, count };
 
-
-// const pool = require("../config/db");
-
-// async function findByEmail(email) {
-//   console.log("[repository] SQL findByEmail:", email);
-  
-//   // Use parameterized queries ($1) to protect against SQL Injection security threats
-//   const query = "SELECT * FROM users WHERE email = $1";
-//   const result = await pool.query(query, [email]);
-  
-//   // return the actual user row, or undefined if no user was found
-//   return result.rows[0]; 
-// }
-
-// async function create(user) {
-//   console.log("[repository] SQL create:", user.email);
-  
-//   const query = "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *";
-//   const values = [user.email, user.password];
-  
-//   const result = await pool.query(query, values);
-//   return result.rows[0];
-// }
-
-// module.exports = { findByEmail, create };
