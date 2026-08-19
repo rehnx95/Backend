ad1d485885ca0e4a19ec71e980b624b00c17d80b
Rehan <rehnx95@gmail.com>
2026-08-19 10:47:09 +0530

    Updating TaskController For Real Database

---
+++ 4 - Architecture/controllers/taskControllers2.js
@@ -0,0 +1,77 @@
+const taskService = require("../services/taskService2");
+const { z, success } = require("zod");
+
+// req.user.id is unique identification of user extracted from login and pass to authenticate funtion that return req.user as decoded
+
+const taskSchema = z.object({
+  title: z.string().min(1, "Title is required"),
+});
+
+async function createTask(req, res) {
+  const result = taskSchema.safeParse(req.body);
+  if (!result.success) {
+    return res.status(400).json({ success: false, value: result.error.issues });
+  }
+
+  const userID = req.user.id;
+  const { title } = result.data;
+
+  const outcome = await taskService.createTask(userID, title);
+  res.status(201).json({
+    success: true,
+    value: `Task Created With Title ${outcome.task.title}`,
+  });
+}
+
+async function getTask(req, res) {
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+
+  const outcome = await taskService.getTask(req.user.id, page, limit);
+
+  res.status(200).json({
+    data: outcome.data,
+    total: outcome.total,
+    page: outcome.page,
+    totalPages: outcome.totalPages,
+  });
+}
+
+async function getoneTask(req, res) {
+  const outcome = await taskService.getoneTask(req.params.id);
+  const task = outcome.task;
+
+  if (!task || task.userID !== req.user.id) {
+    return res.status(404).json({ success: false, value: "Task not found" });
+  }
+  res.status(200).json({ success: true, value: task });
+}
+
+async function updateTask(req, res) {
+  const result = taskSchema.safeParse(req.body);
+  if (!result.success) {
+    return res.status(400).json({ success: false, value: result.error.issues });
+  }
+  const { title } = result.data;
+  const id = req.params.id;
+  const outcome = await taskService.getoneTask(id);
+  const task = outcome.task;
+  if (!task || task.userID !== req.user.id) {
+    return res.status(404).json({ success: false, value: "Task not found" });
+  }
+  const updatedtask = await taskService.updateTask(id, title);
+  res.status(200).json({ success: true, value: updatedtask.task });
+}
+
+async function deleteTask(req, res) {
+  const id = req.params.id;
+  const outcome = await taskService.getoneTask(id);
+  const task = outcome.task;
+  if (!task || task.userID !== req.user.id) {
+    return res.status(404).json({ success: false, value: "Task not found" });
+  }
+  await taskService.deleteTask(id);
+  res.status(204).json();
+}
+
+module.exports = { createTask, getTask, getoneTask, deleteTask, updateTask };
